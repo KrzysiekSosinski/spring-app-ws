fd93bb46ef412ffc7b23ab17e7af769890035770
kris <ksosinski@wp.pl>
2020-10-22 22:24:21 +0200

    initial commit spring project

---
+++ src/test/java/com/kris/SpringAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
